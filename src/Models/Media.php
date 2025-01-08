8cfa3e7872abb08434cecc9b9bf52bd274553c96
balajidharma <balajidharma@hotmail.com>
2025-01-08 18:18:18 -0500

    Added getMediaUrl

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -8,4 +8,13 @@ use Plank\Mediable\Media as MediableMedia;
 class Media extends MediableMedia
 {
     use LaravelCategories;
+
+    public function getMediaUrl()
+    {
+        $stroageFolder = config('media-manager.storage_folder');
+        if ($stroageFolder) {
+            return asset($stroageFolder . '/' . $this->getDiskPath());
+        }
+        return asset($this->getDiskPath());
+    }
 }
